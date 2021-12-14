9d13d6515f939d8b5c2ea40445bfa4438b660602
Tausif Shekh <nisargcontact2020@gmail.com>
2021-12-14 13:54:59 +0530

    object-literal

---
+++ PracticeTest/object-literal.js
@@ -0,0 +1,28 @@
+/* let car = {
+  make: "BMW",
+  model: "75411",
+  year: 2010,
+  getPrice: function () {
+    return 5000;
+  },
+  printDescription: function () {
+    console.log(this.make + " " + this.model);
+  },
+};
+car.printDescription();
+console.log(car.year);
+
+// console.log(car["year"]);
+// console.log(car[1]);
+var anotherCar = {};
+anotherCar.whatever = "boo";
+console.log(anotherCar.whatever);
+
+var a = {
+  myProperty: { b: "hi" },
+};
+console.log(a.myProperty.b);
+ */
+var c = {
+  myProperty: [{ d: "this" }, { e: "can" }, { f: "get" }, { g: "crazy" }],
+};
