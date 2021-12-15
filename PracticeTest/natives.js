3da86c2097487726f2172f2c0c08433df4a0b38e
Tausif Shekh <nisargcontact2020@gmail.com>
2021-12-15 12:48:54 +0530

    native methods, 7 function

---
+++ PracticeTest/natives.js
@@ -0,0 +1,27 @@
+let myString = new String("How are You..?");
+//console.log(myString);
+//console.log(myString.toString());
+//console.log(typeof myString);
+/* 
+String();
+Number();
+Boolean();
+Object();
+Function();
+Symbol() */
+
+/* Array()
+RegExp() */
+
+/* Date()
+Error() */
+
+/* let myPrimitive = "This is crazy";
+myPrimitive = myPrimitive.toUpperCase();
+console.log(myPrimitive);
+ */
+
+let myNumber = new Number();
+console.log(typeof myNumber); //object
+let myPrimitive = myNumber.valueOf();
+console.log(typeof myPrimitive); //number
