ef9ff37303e34389abf55fca726e1cc4499004e3
Tausif Shekh <nisargcontact2020@gmail.com>
2021-12-16 13:43:58 +0530

    strings

---
+++ PracticeTest/strings.js
@@ -0,0 +1,32 @@
+let first = "knowledge is power but enthusiasm pulls the switch";
+let second = "Do or do not, there is no try";
+let third = "4,8,5,5,5,6,4,6,2,1436";
+
+// you can this string literals
+
+// console.log("tausif loves code".toUpperCase());
+
+// let mySpilit = third.split(",");
+// console.log(mySpilit);
+
+// let mySlice = first.slice(10, 20);
+// console.log(mySlice);
+
+// let mySubstr = first.substr(); //two parameter from number length of a number;
+// console.log(mySubstr); // power on the 13 index no. & 5 is a length;
+
+/* let myEndsWith = second.endsWith("try");
+console.log(myEndsWith);
+
+let myStartsWith = second.startsWith("Do");
+console.log(myStartsWith);
+
+let myInclude = second.includes("there");
+console.log(myInclude); */
+
+// let myRepeat = "Ha!!!!!".repeat(3);
+// console.log(myRepeat);
+
+let myTrim = "    bloated            ";
+console.log(myTrim.length); //23
+console.log(myTrim.trim().length); //7
