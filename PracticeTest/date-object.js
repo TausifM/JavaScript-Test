111abf8f5da584691265667ba0951360e80b0bd0
Tausif Shekh <nisargcontact2020@gmail.com>
2021-12-16 13:21:57 +0530

    date-object

---
+++ PracticeTest/date-object.js
@@ -0,0 +1,23 @@
+let today = new Date();
+
+let tausif = new Date("May 23, 1995 12:01:23");
+
+// let tausif = new Date('1995-05-23T07:01:23')
+// let tausif = new Date(1995, 23, 05);
+// let tausif = new Date(1995, 23, 05, 1, 23);
+
+// var elapsedTime = today - tausif;
+// console.log(elapsedTime);
+
+//get parts
+console.log(tausif.getDate()); //23
+
+/* 
+getMonth()
+getDay()
+
+getHours()
+getMinutes()
+getSeconds()
+getMilliseconds() 
+*/
