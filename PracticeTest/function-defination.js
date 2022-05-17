8bfcd06b0a337d9d8a8d6dbfc4b33f1c64ecadb1
TausifM <nisargcontact2020@gmail.com>
2022-05-17 12:51:46 +0530

    Function defination

---
+++ PracticeTest/function-defination.js
@@ -0,0 +1,30 @@
+//=======================FUNCTION DEFINATION=====================
+
+// Before we use a function, we need to define it.
+// A function defination (also called a function declaration, or function statement)
+// consists of the function keyword, followed by:
+
+// The name of the function
+// A listof parameters to the function, enclosed in paraentheses and seperated by commas
+// The JS statements that define the function, enclosed in curly brackets {...}
+
+function functionName(params) {
+    // Statement 
+}
+
+function sun(a, b) { // Parameter
+    // let a =10, b= 20;
+    let total = a+b;
+    console.log(total)
+}
+// Calling function 
+// Define a function does not execute it.
+// A JS function is executed when 'something' invokes it (calls it);
+sum()
+sum(10, 20) //30 // Arguments
+
+// =========FUNCTION PARAMETER VS FUNCTION ARGUMENTS==================
+
+// Function paratmeters are the names listed in the function's defination
+// Function arguments are the rest values passed to the functions
+
