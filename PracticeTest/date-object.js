ff4ebc5ff7098a19bf703fb211634eb476169044
TausifM <nisargcontact2020@gmail.com>
2022-05-25 22:29:55 +0530

    date

--- PracticeTest/date-object.js
+++ PracticeTest/date-object.js
@@ -21,3 +21,23 @@ getMinutes()
 getSeconds()
 getMilliseconds() 
 */
+
+
+// ====================== Date Method and Time Method ==================
+// JS Date Object Represents a single moments in time in a 
+// platform independent format Date Object contains a Number 
+// that represnts milliseconds since January 1, 1970, 00:00:00 UTC
+
+//  Creating Date Object 
+// Their are 4 ways 
+// 1. new Date()
+// 2. new Date(milliseconds)
+// 3. new Date(dateString)
+// 4. new Date(year, month, day, hours, minutes, seconds, milliseconds)
+
+let curTime = new Date();
+console.log(curTime.setTime(5));
+console.log(curTime.setHours(5));
+console.log(curTime.setMinutes(5));
+console.log(curTime.setSeconds(5));
+console.log(curTime.setMilliseconds(5));
